c70ad88a8e6bd5dd691f11e6a837207c0464dc92
ynesss10 <yns10109@gmail.com>
2026-01-27 13:16:51 +0700

    wip: implement student controller

---
+++ app/controllers/StudentController.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Controllers;
+ 
+class StudentController
+{
+    public function index()
+ 
+    {
+     echo '<h1>Daftar Siswa</h1>';
+ 
+     echo '<p>Menampilkan daftar siswa</p>';
+    }
+ 
+    public function create()
+ 
+    {
+     echo '<h1>Tambah Siswa</h1>';
+ 
+     echo '<p>Menampilkan form tambah siswa</p>';
+ 
+    }
+}
+?>
--- app/core/Router.php
+++ app/core/Router.php
@@ -6,6 +6,22 @@ class Router
 {
     public function run()
     {
-        
+      $method = $_SERVER['REQUEST_METHOD'];
+      $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+      
+      if($method == 'GET' && $uri == '/students') {
+         require_once '.app/controllers/StudentController.php';
+        return;
+    }
+
+    if($method == 'GET' && $uri == '/students/create') {
+     
+        return;
+    }
+
+    http_response_code(404);
+    echo '<h1>404 - Page Not Found</h1>';
     }
 }
+    
