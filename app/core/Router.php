0506b2468f9d8c5808e8c632bbbb4d5806e349f9
ynesss10 <yns10109@gmail.com>
2026-01-29 13:57:01 +0700

    wip: implement router app

--- app/core/Router.php
+++ app/core/Router.php
@@ -6,25 +6,46 @@ use App\Controllers\StudentController;
 
 class Router
 {
+    private array $routes = [];
+
+    public function add(string $method, string $uri, string $controller, string $function)
+    {
+        $this->routes[] = [
+            'method' => $method,
+            'uri' => $uri,
+            'controller' => $controller,
+            'function' => $function,
+        ]; 
+    }
+
     public function run()
     {
       $method = $_SERVER['REQUEST_METHOD'];
       $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-      
-      if($method == 'GET' && $uri == '/students') {
-         require_once './app/controllers/StudentController.php';
-         $controller = new StudentController();
-         $controller->index();
-        return;
-    }
+      foreach($this->routes as $router) {
+        $pattern = str_replace(
+            '{id}',
+            '([0-9]+)',
+            $route['uri']
 
-    if($method == 'GET' && $uri == '/students/create') {
-         require_once '.app/controllers/StudentController.php';
-         $controller = new StudentController();
-         $controller->create();
-        return;
-    }
+        );
+
+        $pattern ='#^' . $pattern . '$#';
+
+        if (preg_match($pattern, $uri, $matches)) {
+             require_once './app/controllers/' . $route['controller'] . '.php';
+             array_shift($matches);
+             $controllerClass = 'App\\Controllers\\' . $route['controller'];
+             $controller = new $controllerClass();
+
+             $function = $route['function'];
+             $controller->$function();
+
+             return;
+        }
+        
+      }
 
     http_response_code(404);
     echo '<h1>404 - Page Not Found</h1>';
--- public/index.php
+++ public/index.php
@@ -4,5 +4,10 @@ require_once './app/core/Router.php';
 use App\Core\Router;
 
 $router = new Router();
+
+// Register Routes
+$router->add('GET','/students', 'StudentController', 'Index');
+$router->add('GET','/students/create','StudentController','create');
+$router->add('GET','/students/{id}','StudentController','show');
 $router->run();
 ?>
