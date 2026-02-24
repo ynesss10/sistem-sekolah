2db3e99c51578d95bb653a66d6319e9668fe9060
ynesss10 <yns10109@gmail.com>
2026-02-24 13:17:57 +0700

    wip: setup tailwind

--- app/core/Router.php
+++ app/core/Router.php
@@ -40,7 +40,7 @@ class Router
              $controller = new $controllerClass();
 
              $function = $route['function'];
-             $controller->$function();
+             call_user_func_array([$controller, $function], $matches);
 
              return;
         }
