836733f452deb42ff597834c2545a89585bb7955
ynesss10 <yns10109@gmail.com>
2026-04-07 12:24:13 +0700

    feat: implement submit button and back button in student create page

--- app/views/students/create.php
+++ app/views/students/create.php
@@ -48,6 +48,11 @@
             <label class="block font-bold" for="phone_number">Phone_Number</label>
             <input class="w-full px-4 py-2 border rounded-lg" type="text" id="phone_number" placeholder="Masukkan nomor telepon" name="phone_number">
         </div>
+
+        <div class="flex justify-end col-span-2 gap-4">
+            <a href="/students" class="py-2 px-4 bg-gray-100 rounded-lg">Kembali</a>
+            <button type="submit" class="px-4 py-2 bg-blue-500 rounded-lg text-white">Simpan</button>
+        </div>
     </form>
 
  </div>
