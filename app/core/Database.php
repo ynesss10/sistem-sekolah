b44be11d089a076bf829afea69a9ddde92b70fdd
ynesss10 <yns10109@gmail.com>
2026-04-14 12:30:35 +0700

    feat: implement database layer

---
+++ app/core/Database.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Core
+require_once '../app/config/app.php';
+
+class Database {
+
+
+ protected $connection;
+
+    public function __construct() 
+    {
+      $this->connection = mysqli_connect(
+            DB_HOST,
+            DB_USER,
+            DB_PASSWORD,
+            DB_NAME
+        );
+
+        if(!$this->connection) {
+         die('Error to connect database');
+     }
+    }
+}
+?>
